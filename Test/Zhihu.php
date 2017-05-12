827c6c7adb1c004109abb013e40b499461a0ec6e
liqiang <455019211@qq.com>
2017-05-12 14:11:52 +0800

    1

--- Test/Zhihu.php
+++ Test/Zhihu.php
@@ -28,7 +28,48 @@ class Zhihu
 			$url = 'https://www.zhihu.com/people/' . $uid . '/' . $user_type.'?page='.$page;
 			$result = CurlUtils::zhihuCurl($url);
 			preg_match_all('#<a class="UserLink-link" target="_blank" href="\/people\/(.*?)">(.*?)</a>#', $result, $u_out);
-			print_r($u_out);
+			print_r($url);
+			$data = array();
+			$i = 0;
+			foreach ($u_out[1] as $key => $value) {
+				if ($key%2 == 0&&!empty($value)) {
+					RedisUtil::sadd('request_queue', $value);
+					$data[$i]['uid'] =   $value;
+					$data[$i]['img'] =   $u_out[2][$key];
+					$data[$i]['name'] =   $u_out[2][$key+1];
+					$i++;
+				}
+				# code...
+			}
+			if (count($u_out[1])==36) {
+				$page++;
+				$key = 0;
+			}else{
+				$page= 0;
+				$key = 1;
+			}
+			if (!empty($data)) {
+				ZhUserTABLE::addOne($data);
+			}
+		}
+	}
+	//php G:\nginx\shell\cli.php Zhihu getUser1
+	public static function getUser1(){
+		if (RedisUtil::llen('request_queue') == 0)
+		{
+			RedisUtil::sadd('request_queue', 'li-qiang-2-73');
+		}
+		$user_type = 'following';
+		$page = 0;
+		$key = 1;
+		while (1) {
+			if ($key==1) {
+				$uid = RedisUtil::spop('request_queue');
+			}
+			$url = 'https://www.zhihu.com/people/' . $uid . '/' . $user_type.'?page='.$page;
+			$result = CurlUtils::zhihuCurl($url);
+			preg_match_all('#<a class="UserLink-link" target="_blank" href="\/people\/(.*?)">(.*?)</a>#', $result, $u_out);
+			print_r($url);
 			$data = array();
 			$i = 0;
 			foreach ($u_out[1] as $key => $value) {
