bb126fd525d00e3d4b6897286e7f4fd7e5413e02
liqiang <455019211@qq.com>
2017-05-12 11:20:48 +0800

    1

--- Utils/CurlUtils.php
+++ Utils/CurlUtils.php
@@ -47,7 +47,7 @@ class CurlUtils
 	}
 	private static  function get_rand_ip(){
 	  $arr_1 = array("218","218","66","66","218","218","60","60","202","204","66","66","66","59","61","60","222","221","66","59","60","60","66","218","218","62","63","64","66","66","122","211");
-	  $randarr= mt_rand(0,count($arr_1));
+	  $randarr= mt_rand(0,count($arr_1)-1);
 	  $ip1id = $arr_1[$randarr];
 	  $ip2id=  round(rand(600000,  2550000)  /  10000);
 	  $ip3id=  round(rand(600000,  2550000)  /  10000);
@@ -55,7 +55,7 @@ class CurlUtils
 	  return  $ip1id . "." . $ip2id . "." . $ip3id . "." . $ip4id;
 	}
 	public static function zhihuCurl($url) {
-		
+
 		$ip = self::get_rand_ip();
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_HEADER, 0);
